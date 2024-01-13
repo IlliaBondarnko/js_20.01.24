cdcfa3c081025c3c4c99131a958d598071c5869e
IlliaBondarnko <skyarcher007@gmail.com>
2024-01-13 19:58:29 +0200

    2/7

---
+++ index.js
@@ -0,0 +1,56 @@
+// Напиши функцію logItems(array), яка отримує масив і використовує цикл for, який для кожного елемента масиву буде виводити в консоль повідомлення в форматі [номер елемента] - [значення елемента].
+
+// Нумерація повинна починатися з 1. Наприклад, для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено '1 - Mango', а для індексу 2 виведе '3 - Ajax'.
+
+const logItems = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        console.log(i+1, '-', array[i])
+        
+    }
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// _____________________________________________________________________
+
+
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+    let createArray = message.split(" ")
+    let arrrayLength = createArray.length
+    const result = arrrayLength * pricePerWord
+    // console.log(message)
+    return result
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
